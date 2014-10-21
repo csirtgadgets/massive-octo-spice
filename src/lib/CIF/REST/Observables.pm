0a807eae351cc05b155561b786dacdf6c453e278
Wes Young <wes@barely3am.com>
2014-10-21 08:12:10 -0400

    protecting ->param calls with scalar

--- src/lib/CIF/REST/Observables.pm
+++ src/lib/CIF/REST/Observables.pm
@@ -11,24 +11,26 @@ sub index {
     
     $Logger->debug('generating search...');
     my $res = $self->cli->search({
-        token      	=> $self->param('token'),
-        query      	=> $query,
+        token      	=> scalar $self->param('token'),
+        query      	=> scalar $query,
+        nolog       => scalar $self->param('nolog'),
         filters     => {
-        	otype          	=> $self->param('otype')        || undef,
-        	cc             	=> $self->param('cc')           || undef,
-        	confidence     	=> $self->param('confidence')   || 0,
-        	group          	=> $self->param('group')        || undef,
-        	limit          	=> $self->param('limit')        || undef,
-        	tags           	=> $self->param('tags')         || undef,
-        	application    	=> $self->param('application')  || undef,
-        	asn            	=> $self->param('asn')          || undef,
-        	provider       	=> $self->param('provider')     || undef,
-        	rdata          	=> $self->param('rdata')        || undef,
-        	firsttime      	=> $self->param('firsttime')    || undef,
-        	lasttime		=> $self->param('lasttime')		|| undef,
+        	otype          	=> scalar $self->param('otype')        || undef,
+        	cc             	=> scalar $self->param('cc')           || undef,
+        	confidence     	=> scalar $self->param('confidence')   || 0,
+        	group          	=> scalar $self->param('group')        || undef,
+        	limit          	=> scalar $self->param('limit')        || undef,
+        	tags           	=> scalar $self->param('tags')         || undef,
+        	application    	=> scalar $self->param('application')  || undef,
+        	asn            	=> scalar $self->param('asn')          || undef,
+        	provider       	=> scalar $self->param('provider')     || undef,
+        	rdata          	=> scalar $self->param('rdata')        || undef,
+        	firsttime      	=> scalar $self->param('firsttime')    || undef,
+        	lasttime	    => scalar $self->param('lasttime')     || undef,
+        	reporttime      => scalar $self->param('reporttime')   || undef,
         },
     });
-    $self->stash(observables => $res, token => $self->param('token')); ##TODO is this safe?
+    $self->stash(observables => $res, token => scalar $self->param('token')); ##TODO is this safe?
     $self->respond_to(
         json    => { json => $res },
         html    => { template => 'observables/index' },
@@ -39,7 +41,7 @@ sub show {
     my $self  = shift;
     
     my $res = $self->cli->search({
-        token      => $self->param('token'),
+        token      => scalar $self->param('token'),
         id         => $self->stash->{'observable'},
     });
     $self->stash(observables => $res); ##TODO -- does this leak if we don't clear it?
@@ -53,7 +55,7 @@ sub create {
     my $self = shift;
     
     my $data    = $self->req->json();
-    my $nowait  = $self->param('nowait') || 0;
+    my $nowait  = scalar $self->param('nowait') || 0;
     
     my $res;
     if($nowait){
@@ -106,7 +108,7 @@ sub _submit {
     my $data = shift;
     
     my $res = $self->cli->submit({
-        token           => $self->param('token'),
+        token           => scalar $self->param('token'),
         observables     => $data,
         enable_metadata => 1,
     });
