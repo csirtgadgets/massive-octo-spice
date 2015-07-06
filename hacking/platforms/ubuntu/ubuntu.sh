4307c51a1c63a331acfbfadf1257f615278fa66a
Wes Young <wes@barely3am.com>
2015-07-06 08:31:09 -0400

    fixes #234

--- hacking/platforms/ubuntu/ubuntu.sh
+++ hacking/platforms/ubuntu/ubuntu.sh
@@ -62,32 +62,6 @@ cpanm Search::Elasticsearch@1.19
 echo 'HRNGDEVICE=/dev/urandom' >> /etc/default/rng-tools
 service rng-tools restart
 
-echo 'setting up bind...'
-
-if [ -z `grep -l '8.8.8.8' /etc/bind/named.conf.options` ]; then
-    echo 'overwriting bind config'
-    cp /etc/bind/named.conf.options /etc/bind/named.conf.options.orig
-    cp named.conf.options /etc/bind/named.conf.options
-fi
-
-if [ -z `grep -l 'spamhaus.org' /etc/bind/named.conf.local` ]; then
-    cat ./named.conf.local >> /etc/bind/named.conf.local
-fi
-
-echo 'restarting bind...'
-service bind9 restart
-
-if [ -z `grep -l '^prepend domain-name-servers 127.0.0.1;' /etc/dhcp/dhclient.conf` ]; then
-    cp dhclient.conf /etc/dhcp/
-fi
-
-if [ -z `grep -l '127.0.0.1' /etc/resolvconf/resolv.conf.d/base` ]; then
-    echo 'adding 127.0.0.1 as nameserver'
-    echo "nameserver 127.0.0.1" >> /etc/resolvconf/resolv.conf.d/base
-    echo "restarting network..."
-    ifdown eth0 && sudo ifup eth0
-fi
-
 echo 'setting up apache'
 if [ ! -f /etc/apache2/cif.conf ]; then
     /bin/cp cif.conf /etc/apache2/
@@ -204,6 +178,10 @@ else
 fi
 
 update-rc.d cif-services defaults 99 01
+
+cd hacking/platforms/ubuntu
+bash bind9.sh
+cd ../../../
 
 echo 'staring cif-services...'
 sudo service cif-services start
