19aefcc63c003212aa03e393140a848cb39b1132
Wes Young <wes@barely3am.com>
2015-08-07 08:02:14 -0400

    adding --test-observable, fixing some otype races

--- src/lib/CIF/Smrt.pm
+++ src/lib/CIF/Smrt.pm
@@ -26,7 +26,7 @@ use constant {
     MAX_DT          => '2100-12-31T23:59:59Z' # if you're still using this by then, God help you.
 };
 
-has [qw(ignore_journal config is_test other_attributes test_mode handler rule tmp limit proxy https_proxy)] => (
+has [qw(ignore_journal config is_test other_attributes test_mode handler rule tmp limit proxy https_proxy test_observable)] => (
     is      => 'ro',
 );
 
@@ -141,9 +141,19 @@ sub process {
         $data = [ @$data[0..($self->limit-1)] ];   
     }
     
+    if($self->test_observable){
+        $Logger->info('testing observable: ' . $self->test_observable);
+    }
+    
     foreach (@$data){
+        if($self->test_observable){
+            next unless($_->{'observable'} && $_->{'observable'} eq $self->test_observable);
+        }
+        
         $otype = observable_type($_->{'observable'});
         next unless($otype);
+        $_->{'otype'} = $otype unless($_->{'otype'});
+        
         
         $_->{'reporttime'} = $reporttime unless($_->{'reporttime'});
 
@@ -152,7 +162,7 @@ sub process {
         
         next unless($self->not_before <= $ts );
         $_ = $self->rule->process({ data => $_ });
-        $_->{'otype'} = $otype unless($_->{'otype'});
+        
         local $Data::Dumper::Indent = 0;
 
         push(@array,$_);
