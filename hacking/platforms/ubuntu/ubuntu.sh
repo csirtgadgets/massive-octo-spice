d045a16886696868e393338304930c21aa242b08
Wes Young <wes@barely3am.com>
2015-12-31 12:12:38 -0500

    travis

--- hacking/platforms/ubuntu/ubuntu.sh
+++ hacking/platforms/ubuntu/ubuntu.sh
@@ -52,16 +52,13 @@ bash firewall.sh
 
 echo 'installing cpanm...'
 curl -L https://cpanmin.us | sudo perl - App::cpanminus
-alias cpanm='cpanm --wget --mirror https://cpan.metacpan.org'
+alias cpanm='cpanm --wget --mirror https://cpan.metacpan.org --skip-installed'
 
+cpanm Regexp::Common
 cpanm Moo@1.007000
 cpanm Mouse@2.4.1
 cpanm ZMQ::FFI@0.17
 cpanm --force --notest https://github.com/csirtgadgets/ZMQx-Class/archive/master.tar.gz
-
-alias cpanm='cpanm --wget --mirror https://cpan.metacpan.org --skip-installed'
-
-cpanm Regexp::Common
 cpanm Log::Log4perl@1.44
 cpanm Test::Exception@0.32
 cpanm MaxMind::DB::Reader@0.050005
